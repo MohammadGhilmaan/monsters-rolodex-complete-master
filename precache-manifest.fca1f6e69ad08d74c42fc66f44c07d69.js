b61ad416ed01d5d235fa7118e7a01831ae913457
unknown <Ghilmaanshahzad@outlook.com>
2021-12-31 17:04:56 +0500

    Updates

---
+++ precache-manifest.fca1f6e69ad08d74c42fc66f44c07d69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f570abd3b1e78570bf6a1ad9fa4bd36",
+    "url": "/monsters-rolodex-complete-master/index.html"
+  },
+  {
+    "revision": "87f86834ebbcc0b9c89b",
+    "url": "/monsters-rolodex-complete-master/static/css/main.f251e27e.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex-complete-master/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex-complete-master/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87f86834ebbcc0b9c89b",
+    "url": "/monsters-rolodex-complete-master/static/js/main.98d1ef0b.chunk.js"
+  },
+  {
+    "revision": "fbe0a9bb9a48b2b784c0",
+    "url": "/monsters-rolodex-complete-master/static/js/runtime-main.a65b7d57.js"
+  }
+]);
